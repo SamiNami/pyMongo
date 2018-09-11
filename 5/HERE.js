4b967f7eb0365d68b85154f4d9524dab3ae07c56
sami peralahti <sami.peralahti@vainu.io>
2018-09-10 17:41:30 -0700

    final thingy done

--- 5/HERE.js
+++ 5/HERE.js
@@ -146,3 +146,19 @@ db.grades.aggregate([
     $sort: { avg: -1 }
   }
 ]);
+
+db.zips.aggregate([
+  {
+    $project: {
+      first_char: { $substr: ["$city", 0, 1] },
+      pop: 1
+    }
+  },
+  { $match: { first_char: { $in: ["B", "D", "O", "G", "N", "M"] } } },
+  {
+    $group: {
+      _id: "",
+      sum: { $sum: "$pop" }
+    }
+  }
+]);
